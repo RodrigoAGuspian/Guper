4118c31b6f5659f9b6d09a9285ccac638e15d48e
RodrigoAGuspian <37385496+RodrigoAGuspian@users.noreply.github.com>
2018-05-29 09:57:30 -0500

    commit7

--- ProyectoSena/ProyectoSena/settings.py
+++ ProyectoSena/ProyectoSena/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ProyectoSena.urls'
@@ -143,4 +144,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT ='staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
